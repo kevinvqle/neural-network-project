706cb03a788f2908b076a171ecd9cf35ea028ddc
Kevin Le <kevin.lelele@csu.fullerton.edu>
2022-04-17 01:01:22 -0700

    Update neural-network.py

--- neural-network.py
+++ neural-network.py
@@ -28,6 +28,11 @@ class Loss:
         data_loss = np.mean(sample_losses)
         return data_loss
 
+class Loss_CatergoricalCrossentropy(Loss):
+    def forward(self,y_pred,y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7,1-1e-7)
+
 X,y = spiral_data(samples=100, classes = 3)
 
 dense1 = Layer_Dense(2,3)
