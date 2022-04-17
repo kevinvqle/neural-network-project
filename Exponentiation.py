96df9d91a65fcad6c52c6f5583340bcac8f9508b
Kevin Le <kevin.lelele@csu.fullerton.edu>
2022-04-16 23:50:01 -0700

    import numpy

--- Exponentiation.py
+++ Exponentiation.py
@@ -1,18 +1,14 @@
 import math 
 import numpy as np
+import nnfs 
+nnfs.init()
 
 layer_outputs = [4.8, 1.21, 2.385]
 
 E = math.e
 exp_values = np.exp(layer_outputs)
 
-print(exp_values)
+norm_values = exp_values / np.sum(exp_values)
 
-norm_base = sum(exp_values)
-norm_values = []
-
-for value in exp_values:
-    norm_values.append(value / norm_base)
-
-print(norm_base)
+print(norm_values)
 print(sum(norm_values))
