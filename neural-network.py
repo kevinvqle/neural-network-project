28fbbefdc00c56d91b3f01396c9cefacf662a2e9
Kevin Le <kevin.lelele@csu.fullerton.edu>
2022-04-17 01:04:49 -0700

    Update neural-network.py

--- neural-network.py
+++ neural-network.py
@@ -32,6 +32,10 @@ class Loss_CatergoricalCrossentropy(Loss):
     def forward(self,y_pred,y_true):
         samples = len(y_pred)
         y_pred_clipped = np.clip(y_pred, 1e-7,1-1e-7)
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples),y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis = 1)
 
 X,y = spiral_data(samples=100, classes = 3)
 
