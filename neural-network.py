a9a5ae1e5edd03964fade4e82589a3f55669d26f
Kevin Le <kevin.lelele@csu.fullerton.edu>
2022-04-17 00:12:01 -0700

    created data set by using a set of 3

--- neural-network.py
+++ neural-network.py
@@ -5,12 +5,6 @@ from nnfs.datasets import spiral_data
 
 nnfs.init()
 
-X = [[1, 2, 3, 2.5],
-        [2.0,5.0,-1.0,2.0],
-        [-1.5, 2.7, 3.3, -0.8]]
-
-X, y = spiral_data(100,3)
-
 class Layer_Dense: #used a random.seed to produce a random values for my array 
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs,n_neurons) 
@@ -22,13 +16,40 @@ class Activation_ReLU:
     def forward(self,inputs):
         self.output = np.maximum(0, inputs)
 
-layer1 = Layer_Dense(2,5)  #parameters: (size of input, how many neurons we have)
-#layer2 = Layer_Dense(5,2) # the input of layer2 has to be the output of layer1
+class Activation_Softmax:
+    def forward(self,inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis = 1, keepdims = True))
+        probabilities = exp_values / np.sum(exp_values, axis = 1, keepdims = True)
+        self.output = probabilities
+
+X,y = spiral_data(samples=100, classes = 3)
+
+dense1 = Layer_Dense(2,3)
 activation1 = Activation_ReLU()
-layer1.forward(X)
-#print(layer1.output)
-activation1.forward(layer1.output) #optimized values -> removed negative numbers 
-print(activation1.output)
+
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #understand the difference between ReLU activation and linear activation 
 
