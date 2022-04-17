3761e8dbf6ff07a2108956effcb7d925a4cc161a
Kevin Le <kevin.lelele@csu.fullerton.edu>
2022-04-16 23:48:14 -0700

    Exponentiation code

--- Exponentiation.py
+++ Exponentiation.py
@@ -1,5 +1,18 @@
 import math 
+import numpy as np
 
 layer_outputs = [4.8, 1.21, 2.385]
 
 E = math.e
+exp_values = np.exp(layer_outputs)
+
+print(exp_values)
+
+norm_base = sum(exp_values)
+norm_values = []
+
+for value in exp_values:
+    norm_values.append(value / norm_base)
+
+print(norm_base)
+print(sum(norm_values))
