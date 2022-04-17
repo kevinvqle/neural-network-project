312fe946ad3f8be90f69cf06ffc695ddae6a16e5
Kevin Le <kevin.lelele@csu.fullerton.edu>
2022-04-16 20:14:47 -0700

    created classes

--- neural-network.py
+++ neural-network.py
@@ -1,29 +1,81 @@
 import numpy as np
 
-inputs = [[1, 2, 3, 2.5],
+np.random.seed(0)
+
+X = [[1, 2, 3, 2.5],
         [2.0,5.0,-1.0,2.0],
         [-1.5, 2.7, 3.3, -0.8]]
 
+class Layer_Dense: #used a random.seed to produce a random values for my array 
+    def __init__(self,n_inputs, n_neurons):
+        self.weights = np.random.randn(n_inputs,n_neurons) 
+        self.biases = np.zeros(1,n_neurons) 
+    def foward(self):
+        pass
 
-
-weights = [[0.2, 0.8, -0.5, 1],
-           [0.5, -0.91, 0.26, -0.5],
-          [-0.26, -0.27, 0.17, 0.87]]
-
-biases = [2,3,0.5]
-
-weights2 = [[0.1, -0.14, 0.5],
-           [-0.5,0.12,-0.33],
-          [-0.44, 0.73, -0.13]]
-
-biases2 = [-1,2,-0.5]
+print(0.10*np.random.randn(4,3))
 
 
 
-layer1_output = np.dot(inputs, np.array(weights).T) + biases # ultilized transpose in order to mulitply by a 4x4 matricies 
 
-print (layer1_output)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#weights = [[0.2, 0.8, -0.5, 1],
+#           [0.5, -0.91, 0.26, -0.5],
+#         [-0.26, -0.27, 0.17, 0.87]]
+
+#biases = [2,3,0.5]
+
+#weights2 = [[0.1, -0.14, 0.5],
+ #          [-0.5,0.12,-0.33],
+    #      [-0.44, 0.73, -0.13]]
+
+#biases2 = [-1,2,-0.5]
+
+
+
+#layer1_output = np.dot(inputs, np.array(weights).T) + biases # ultilized transpose in order to mulitply by a 4x4 matricies 
+#layer2_output = np.dot(layer1_output, np.array(weights2).T) + biases2
+#print(layer2_output)
 
 
 
