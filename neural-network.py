efad232b3ac5ee03f3c98ff8e8ae3507fbc930b2
Kevin Le <kevin.lelele@csu.fullerton.edu>
2022-04-17 00:57:45 -0700

    created a loss class

--- neural-network.py
+++ neural-network.py
@@ -22,6 +22,10 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis = 1, keepdims = True)
         self.output = probabilities
 
+class Loss: 
+    def calculate(self,output,y):
+        sample_losses = self.forward(output,y)
+
 X,y = spiral_data(samples=100, classes = 3)
 
 dense1 = Layer_Dense(2,3)
