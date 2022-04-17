616c419f7169950bedadbeef9d31f16fc5c163cf
Kevin Le <kevin.lelele@csu.fullerton.edu>
2022-04-16 20:35:34 -0700

    Update neural-network.py

--- neural-network.py
+++ neural-network.py
@@ -1,3 +1,4 @@
+from doctest import OutputChecker
 import numpy as np
 
 np.random.seed(0)
@@ -7,14 +8,19 @@ X = [[1, 2, 3, 2.5],
         [-1.5, 2.7, 3.3, -0.8]]
 
 class Layer_Dense: #used a random.seed to produce a random values for my array 
-    def __init__(self,n_inputs, n_neurons):
-        self.weights = np.random.randn(n_inputs,n_neurons) 
-        self.biases = np.zeros(1,n_neurons) 
-    def foward(self,inputs):
-        self.output = np.dot(inputs, self.weights + self.biases) # calculate the output using dot product
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs,n_neurons) 
+        self.biases = np.zeros((1, n_neurons))
+    def foward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases # calculate the output using dot product
 
 layer1 = Layer_Dense(4,5)  #parameters: (size of input, how many neurons we have)
-layer2 = Layer_Dense(5,5) # the input of layer2 has to be the output of layer1
+layer2 = Layer_Dense(5,2) # the input of layer2 has to be the output of layer1
+
+layer1.foward(X)
+print(layer1.output)
+layer2.foward(layer1.output)
+print(layer2.output)
 
 
 
