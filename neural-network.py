4a221cd9a8b1dd1796a10b61929bcbb62155f25e
Kevin Le <kevin.lelele@csu.fullerton.edu>
2022-04-16 19:18:05 -0700

    Update neural-network.py

--- neural-network.py
+++ neural-network.py
@@ -8,7 +8,8 @@ weights = [0.2,0.8,-0.5,1]
 
 bias = 2
 
-
+output = np.dot(inputs,weights) + bias 
+print (output)
 #weights = [[0.2,0.8,-0.5,1],
 #            [0.5, -0.91, 0.26, -0.5],
 #            [-0.26, -0.27, 0.17, 0.87]
