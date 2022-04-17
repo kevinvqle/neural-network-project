11cc413368fca5036d492062e28aa2c6b7c03050
Kevin Le <kevin.lelele@csu.fullerton.edu>
2022-04-16 19:21:25 -0700

    swapped weights and inputs

--- neural-network.py
+++ neural-network.py
@@ -8,9 +8,9 @@ weights = [[0.2, 0.8, -0.5, 1],
            [0.5, -0.91, 0.26, -0.5],
           [-0.26, -0.27, 0.17, 0.87]]
 
-bias = 2
+biases = [2,3,0.5]
 
-output = np.dot(inputs,weights) + bias 
+output = np.dot(weights,inputs) + biases
 print (output)
 
 
